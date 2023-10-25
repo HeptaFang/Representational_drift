172f86ff10f45cf6a2a0980052604a4704e4f31a
HeptaFang <SeptaFang@gmail.com>
2023-10-25 12:15:29 -0500

    Add main.py

---
+++ main.py
@@ -0,0 +1,13 @@
+from path_check import main as path_check
+from training import main as train
+
+
+def main():
+    """
+    Hub script. Run all the processes in proper order.
+    """
+    path_check()
+
+
+if __name__ == '__main__':
+    main()
