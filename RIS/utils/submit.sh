666397ca42398498b786a9d766ac4f4a5368e1a5
HeptaFang <SeptaFang@gmail.com>
2023-10-25 14:13:59 -0500

    RIS test

--- RIS/utils/submit.sh
+++ RIS/utils/submit.sh
@@ -1,1 +1,1 @@
-bsub -q general -n 1 -R 'rusage[mem=4GB] span[hosts=1]' -a 'docker(continuumio/anaconda3)' training.sh
+bsub -q general -n 1 -R 'rusage[mem=4GB] span[hosts=1]' -a 'docker(continuumio/anaconda3)' -G 'compute-hiratani' bash training.sh
